a4935874ee8435a7575f20e7236aced2438c1878
wndnjs00 <juwonjeon852@gmail.com>
2024-12-29 10:59:19 +0900

    주석삭제

--- app/src/main/java/com/example/crudapplication/UserAdapter.java
+++ app/src/main/java/com/example/crudapplication/UserAdapter.java
@@ -30,7 +30,6 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserViewHolder
     @NonNull
     @Override
     public UserViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        // item.xml 따로생성해서 수정하기(임시)
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.user_list_item, parent, false);
         return new UserViewHolder(view);
     }
