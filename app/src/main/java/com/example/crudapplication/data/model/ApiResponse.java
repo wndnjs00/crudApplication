95290267e54ede346ae879418176801bcce98cbb
wndnjs00 <juwonjeon852@gmail.com>
2025-02-22 16:59:37 +0900

    feat: 전체적인 코드수정

---
+++ app/src/main/java/com/example/crudapplication/data/model/ApiResponse.java
@@ -0,0 +1,32 @@
+package com.example.crudapplication.data.model;
+
+public class ApiResponse<T> {
+    private String status;
+    private String message;
+    private T data;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+}
